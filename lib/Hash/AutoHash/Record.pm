2d91eeb6aa2e1f154e32f8404c21c4c9982eae6b
Nathan Goodman <natg@cpan.org>
2012-09-03 00:49:41 +0000

    Import of NATG/Hash-AutoHash-Record-1.13 from CPAN.
    gitpan-cpan-distribution: Hash-AutoHash-Record
    gitpan-cpan-version:      1.13
    gitpan-cpan-path:         NATG/Hash-AutoHash-Record-1.13.tar.gz
    gitpan-cpan-author:       NATG
    gitpan-cpan-maturity:     released

--- lib/Hash/AutoHash/Record.pm
+++ lib/Hash/AutoHash/Record.pm
@@ -1,5 +1,5 @@
 package Hash::AutoHash::Record;
-our $VERSION='1.12';
+our $VERSION='1.13';
 $VERSION=eval $VERSION;		# I think this is the accepted idiom..
 
 #################################################################################
@@ -27,6 +27,7 @@ our @SUBCLASS_EXPORT_OK=$helper_class->SUBCLASS_EXPORT_OK;
 # functions herein (except _new) are exportable by Hash::AutoHash::Args
 #################################################################################
 package Hash::AutoHash::Record::helper;
+our $VERSION='1.13';
 use strict;
 use Carp;
 BEGIN {
@@ -49,7 +50,7 @@ sub autohash_clear {
 # Tied hash which implements Hash::AutoHash::Record
 #################################################################################
 package Hash::AutoHash::Record::tie;
-our $VERSION=$Hash::AutoHash::Record::VERSION;
+our $VERSION='1.13';
 use strict;
 use Carp;
 use Tie::Hash;
@@ -323,7 +324,7 @@ Hash::AutoHash::Record - Object-oriented access to hash with implicitly typed fi
 
 =head1 VERSION
 
-Version 1.12
+Version 1.13
 
 =head1 SYNOPSIS
 
