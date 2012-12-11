8aee27c5c5070a506e5640e6eb2fa6f42273004a
Nathan Goodman <natg@cpan.org>
2012-12-11 21:07:14 +0000

    Import of NATG/Hash-AutoHash-Record-1.16 from CPAN.
    gitpan-cpan-distribution: Hash-AutoHash-Record
    gitpan-cpan-version:      1.16
    gitpan-cpan-path:         NATG/Hash-AutoHash-Record-1.16.tar.gz
    gitpan-cpan-author:       NATG
    gitpan-cpan-maturity:     released

--- lib/Hash/AutoHash/Record.pm
+++ lib/Hash/AutoHash/Record.pm
@@ -1,5 +1,5 @@
 package Hash::AutoHash::Record;
-our $VERSION='1.16_01';
+our $VERSION='1.16';
 $VERSION=eval $VERSION;		# I think this is the accepted idiom..
 
 #################################################################################
@@ -324,7 +324,7 @@ Hash::AutoHash::Record - Object-oriented access to hash with implicitly typed fi
 
 =head1 VERSION
 
-Version 1.16_01
+Version 1.16
 
 =head1 SYNOPSIS
 
